f7dcac719272ed77eb1db31cbeb3a8afbb5dda3c
Varga Balázs Ádám <112722847+VargaBalazsAdam@users.noreply.github.com>
2023-09-19 20:11:00 +0200

    xaml

---
+++ Graph-Plotter/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using MathNet.Numerics;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+using org.mariuszgromada.math.mxparser;
+using Expression = org.mariuszgromada.math.mxparser.Expression;
+
+namespace Graph_Plotter
+{
+  public partial class MainWindow : System.Windows.Window
+    {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+
+
+
+  }
+}
